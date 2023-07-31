c487e67cbc1cd00569f7daade59b66eda1b8a8b1
Anna Baraulina <a.baraulina@yandex.ru>
2023-07-31 12:58:11 +0300

    before init git

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import { ChangeEvent, FormEvent, useState, FC} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import style  from './fibonacci-page.module.css';
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { setDelay } from "../../utils/set-delay";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { nanoid } from "nanoid";
+
+
+
+const fibonacci = (n: number): number[] => {
+  let arr: number[] = [1, 1];
+  for ( let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr;
+};
 
 export const FibonacciPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [loader, setLoader] = useState(false);
+  const [array, setArray] = useState<Array<number>>();
+
+  const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(evt.target.value)
+  };
+
+  const onSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    setLoader(true);
+    const data = fibonacci(Number(inputValue));
+    for (let i = 0; i < data.length; i++) {
+      await setDelay(SHORT_DELAY_IN_MS);
+      setArray(data.slice(0, i + 1));
+    }
+    setInputValue('');
+    setLoader(false);
+  }
+
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+     <form className={style.form} onSubmit={onSubmit}>
+      <Input type='number' onChange={onChange} value={inputValue} min={1} max={19}/>
+      <Button text='рассчитать' isLoader={loader} type='submit' disabled={!!!inputValue || Number(inputValue) > 19 || Number(inputValue) < 1}/>
+     </form>
+     <ul className={style.list}>
+      {array?.map((item, index) => {
+        return <Circle letter={String(item)} index={index} key={nanoid()}/>
+      })}
+     </ul>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,89 @@
-import React from "react";
+import { ChangeEvent, FC, FormEvent, useState}from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { ElementTypes } from "../../types/element-states";
+import { DELAY_IN_MS } from "../../constants/delays";
+import { nanoid } from 'nanoid';
+import style from './string.module.css';
+
+export const swap = (
+  value: ElementTypes[],
+  firstItem: number,
+  secondItem: number
+) => {
+  return ([value[firstItem], value[secondItem]] = [
+    value[secondItem],
+    value[firstItem],
+  ])
+}
+
+export const swapReverse = (array: string) => {
+  const newArray = array.split('');
+  const end = newArray.length - 1;
+  const mid = Math.ceil(newArray.length / 2);
+  for (let i = 0; i < mid; i++) {
+    let j = end - i;
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
+
+export function setDelay(delay: number) {
+  return new Promise<void>((res) => setTimeout(res, delay));
+}
 
 export const StringComponent: React.FC = () => {
+  const [array, setArray] = useState<Array<ElementTypes>>();
+  const [inputValue, setInputValue] = useState('');
+  const [loader, setLoader] = useState(false);
+
+  const onChange = ( e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newArray = inputValue.split('').map((letter: string) => {
+      return { letter, state: ElementStates.Default};
+    });
+    setArray(newArray);
+    setLoader(true);
+    const end = newArray.length - 1;
+    const middle = Math.ceil(newArray.length / 2);
+
+    for (let i = 0; i < middle; i++) {
+      let j = end - i;
+      if ( i !== j) {
+        newArray[i].state = ElementStates.Changing;
+        newArray[j].state = ElementStates.Changing;
+        setArray([...newArray]);
+        await setDelay(DELAY_IN_MS);
+      }
+      swap(newArray, i, j);
+      newArray[i].state = ElementStates.Modified;
+      newArray[j].state = ElementStates.Modified;
+
+      setArray([...newArray]);
+    }
+    setLoader(false);
+    setInputValue('');
+  };
+
+
   return (
     <SolutionLayout title="Строка">
-     
+     <form className={style.form} onSubmit={onSubmit}>
+       <Input value={inputValue} onChange={onChange} maxLength={11} isLimitText/>
+       <Button text='развернуть' type='submit' isLoader={loader} disabled={!!!inputValue}/>
+     </form>
+     <ul className={style.list}>
+      {array?.map((item) => (
+        <Circle letter={item.letter} state={item.state} key={nanoid()}/>
+      ))}
+     </ul>
     </SolutionLayout>
   );
 };
